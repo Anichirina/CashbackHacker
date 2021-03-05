8470a8442440fb64b37d5183207c3e80310e6110
Ira Anichkova <anichkova2016@yandex.ru>
2021-03-05 23:28:11 +0300

    change file build.gradle, CashbackHackerTest

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -1,37 +1,40 @@
 package ru.netology.service;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CashbackHackerTest {
     CashbackHacker cashbackHacker = new CashbackHacker();
 
-    @org.junit.Test
+    @Test
     public void shouldReturn1000() {
                assertEquals(0, cashbackHacker.remain(1000));
 
     }
 
-    @org.junit.Test
+    @Test
     public void shouldReturn100For900() {
 
         assertEquals(100, cashbackHacker.remain(900));
 
     }
 
-    @org.junit.Test
+    @Test
     public void shouldReturn50for950() {
         assertEquals(950, cashbackHacker.remain(50));
     }
 
-    @org.junit.Test
+    @Test
     public void shouldReturnLessOf1000() {
 
         assertEquals(500, cashbackHacker.remain(500));
 
     }
 
-    @org.junit.Test
+    @Test
     public void shouldReturnNull() {
                 assertEquals(1000, cashbackHacker.remain(0));
 
